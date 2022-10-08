dedc3a2feb95145ae9a918833998132bd95174bb
Tariel98 <tarielarakelyan929@gmail.com>
2022-10-09 02:37:56 +0400

    all files moved in ITC-lessons dir

--- ITC_lessons/testt.py
+++ ITC_lessons/testt.py
@@ -1,9 +1,15 @@
-print('Prime numbers between 900 and 1000 are:')
+#!/bin/python3
 
-for x in range(21):
-	if x >1:
-		for num in (2,x):
-			if x % num == 0:
-				break
-			else:
-				print(x)
+#
+#a = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5}
+#print(type(a))
+#
+
+tiket_number = int(input('Type your ticket number: '))
+summ_first = (tiket_number // 100000) + (tiket_number // 10000 % 10) + (tiket_number // 1000 % 10) 
+summ_end   = (tiket_number // 100 % 10) + (tiket_number // 10 % 10) + (tiket_number % 10) 
+while summ_first != summ_end:
+	tiket_number = int(input('Type your ticket number: '))    
+	summ_first = (tiket_number // 100000) + (tiket_number // 10000 % 10) + (tiket_number // 1000 % 10) 
+	summ_end   = (tiket_number // 100 % 10) + (tiket_number // 10 % 10) + (tiket_number % 10) 	
+print('You win!')
