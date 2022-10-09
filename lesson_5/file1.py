e72c710a66625edb6da3786db6a0198407bf196a
Tariel98 <tarielarakelyan929@gmail.com>
2022-10-09 17:37:51 +0400

    aded new class Number

--- lesson_5/file1.py
+++ lesson_5/file1.py
@@ -13,3 +13,33 @@ odd_nums = generate_odd_nums()
 
 for x in odd_nums:
 	print(x)
+
+
+class Number:
+	
+	def __init__(self, number):
+		self.number = number
+
+	def odd_number(self):
+		my_odd_list  = []
+		for x in range(self.number):
+			if x % 2 == 0:
+				my_odd_list.append(x)
+#			else:
+#				continue
+		return my_odd_list
+
+	def env_number(self):
+		my_env_list = []
+		for x in range(self.number):
+			if x % 2 != 0:
+				my_env_list.append(x)
+#			else:
+#				continue
+		return my_env_list
+
+	def __del__(self):
+
+odd = Number(50)
+print(odd.odd_number())
+print(odd.env_number())
