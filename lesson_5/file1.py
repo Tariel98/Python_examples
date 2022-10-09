24ab6a91c98535d5ee12dd0e54fdf09b55895b3d
Tariel98 <tarielarakelyan929@gmail.com>
2022-10-09 17:10:43 +0400

    aded new genereator function in file1.py

--- lesson_5/file1.py
+++ lesson_5/file1.py
@@ -1,2 +1,14 @@
 def printer():
 	print('This is in file1.py')
+
+def generate_odd_nums():
+	for i in range(50):
+		if i % 2 != 0:
+			yield i
+
+
+
+odd_nums = generate_odd_nums()
+
+for x in odd_nums:
+	print(x)
