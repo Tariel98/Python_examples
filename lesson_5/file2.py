5b66762f56547f72823bac738387cc09de085a72
Tariel98 <tarielarakelyan929@gmail.com>
2022-10-09 16:05:56 +0400

    uncomented file2.py and aded new functions

--- lesson_5/file2.py
+++ lesson_5/file2.py
@@ -1,15 +1,24 @@
 from file1 import printer
 
-#def printer():
-#	print('in file2.py')
-#
-##printer()
-##def main():
-##	printer()
- #
+def printer():
+	print('in file2.py')
+
+
+printer()
+
+
+def main():
+	printer()
+
 printer()
 print(__name__)
-#if __name__ == '__main__':
-#	print('this is file1.py')
-#else:
-#	print(__name__)
+
+
+
+
+
+
+if __name__ == '__main__':
+	print('this is file1.py')
+else:
+	print(__name__)
